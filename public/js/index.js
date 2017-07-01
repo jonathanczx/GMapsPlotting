be412a66e89686961f8439e43f7b512469d89b6d
Jonathan Chan <jonathanczx@gmail.com>
2017-07-02 00:05:17 +1000

    Plot maps working

--- public/js/index.js
+++ public/js/index.js
@@ -9,19 +9,15 @@ $(document).ready(function(){
 
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCHclD71jH3xZRMkSDnopDC__RRHroaQg4&callback=initMap';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCHclD71jH3xZRMkSDnopDC__RRHroaQg4';
     document.getElementsByTagName('head')[0].appendChild(js_file);
-  }
-
+  } 
 
   $("#csvFile").change(function (e){
     $("#status").empty().text("File is uploading... ");
     var file = e.target.files[0];
     var formData = new FormData();
     formData.append("file", file);
-
-
-
 
     //make ajax post call 
     $.ajax({
@@ -30,22 +26,21 @@ $(document).ready(function(){
       data: formData,
       processData: false,
       contentType: false,
-
       success: function(res){
         //successful request
         $("#status").empty().text(res);
         callback(res);
+        initMap();
       },
       error: function(error){
         $("#status").empty().text(error);
       }
     });
-
-
-
-    
-  });
+  }); 
 });
+
+
+
 var map;
 var jobID;
 var jsonData;
@@ -60,6 +55,7 @@ function initMap(){
     };
   map = new google.maps.Map(document.getElementById('map'), options);
   
+  addrToLatLng(jsonData, jobID);
 };
 
 //convert address to coordinates
@@ -72,43 +68,28 @@ function addrToLatLng(address, response){
     var addr = jsonAddress[i].address;
     geocoder.geocode({'address': addr}, function(results, status){
       if (status === 'OK'){
-        var myObj = {
-          "result" : results
-        };
-        response.push(myObj);            
+        console.log(results);
+        markers.push(
+          new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: map,
+            animation: google.maps.Animation.DROP
+          }));           
       } else {
         console.log('geocode was not successful: ' + status);
       }
     });
   };
   console.log(response);
-  plotMarkers(response);
+  callbackGeocode(response);
 };
 
 //callback function to store ajax response in jsonData
 function callback(response){
   jsonData = response;
-  addrToLatLng(jsonData, jobID);
 }
 
-//function to plotMarkers
-function plotMarkers(resultInput){
-  console.log(resultInput);
-  for (var x = 0; x < resultInput.length; x++){
-    var lat = resultInput[x].result.results[0].geometry.location.lat;
-    var lng = resultInput[x].result.results[0].geometry.location.lng;
-    var latlng = new google.maps.LatLng(lat, lng);
-    console.log(latlng);
-    markers.push(
-      new google.maps.Marker({
-        position: latlng,
-        map: map,
-        animation: google.maps.Animation.DROP
-      })
-    );
-  };
-};
-// function geocodeCallbackResult(response){
-//   jobID = response;
-// }
+function callbackGeocode(response){
+  jobID = response;
+}
 
